46fc503a5dc51b0ab0e548bcc4f5ab7cac08a68b
zainabasif23 <145759714+zainabasif23@users.noreply.github.com>
2024-01-07 00:13:04 +0530

    created login and signup by zainab asif

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,21 +4,24 @@ import styles from '../page.module.css'
 const Login = () => {
   return (
     <main className='main'>
-      <h1>Login</h1>
-      <form>
-      <div className="container">
-        <div className="form.box">
-          
-
-
-
-        </div>
-      </div>
-
-      </form>
-      
-    </main>
-  )
+      <div className="Login">
+        <div className="formbox">
+          <h1>Login</h1>
+          <form>
+            <div className="input-login1">
+              <div className="field2">
+                <input type="text" placeholder="Email" name="email" required><br><br>
+                  <input type="password" placeholder="Password">
+                  </div>
+                  <div className="btn2">
+                    <button type="button">Login</button>
+                  </div>
+                </div>
+                </form>
+              </div>
+            </div>
+          </main>
+          )
 }
 
-export default Login
+          export default Login
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -4,12 +4,28 @@ import styles from '../page.module.css'
 const Signup = () => {
   return (
     <main className='main'>
-      </main>
+      <div className="signupcontainer">
+        <div className="form-box">
+          <h1>SignUp</h1>
+          <form>
+            <div className="input-login">
+              <div className="field">
+                <input type="text" placeholder="Email" name="email" required><br><br>
+                  <input type="password" placeholder="Password">
+                  </div>
+                  <div className="btn">
+                    <button typeName="button">Sign Up</button>
+                  </div>
+                </div>
+                </form>
+              </div>
+            </div>
+          </main>
 
 
 
 
-  )
+          )
 }
 
-export default Signup
+          export default Signup
