e4e65110168d83f18782fdc362b388bfc5b541a8
Khushi Chaudhary <khushichdry2912@gmail.com>
2024-01-07 14:12:24 +0530

    TrackCard by Khushi

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,9 +1,32 @@
 import React from 'react'
-import styles from '../app/page.module.css' 
+import styles from '../app/page.module.css'
+import { LiaShoppingBagSolid } from "react-icons/lia";
+import { LiaShippingFastSolid } from "react-icons/lia";
+import { HiOutlineCurrencyRupee } from "react-icons/hi2";
 
 const Track = () => {
   return (
-    <main className='main'>Track</main>
+    <main className='main'>
+      <div className={styles.maintrack}>
+        <div className={styles.TrackBag}>
+          <p className={styles.TrackBaglogo}><LiaShoppingBagSolid /></p>
+          <h3 className={styles.TrackBagHead}>Premium Tshirts</h3>
+          <p className={styles.TrackBagContent}>Our T-Shirts are 100% made of cotton.</p>
+        </div>
+        <div className={styles.TrackBag}>
+          <p className={styles.TrackBaglogo}><LiaShippingFastSolid /></p>
+          <h3 className={styles.TrackBagHead}>Free Shipping</h3>
+          <p className={styles.TrackBagContent}>We ship all over India for FREE.</p>
+        </div>
+        <div className={styles.TrackBag}>
+          <p className={styles.TrackBaglogo}><HiOutlineCurrencyRupee /></p>
+          <h3 className={styles.TrackBagHead}>Exciting Offers</h3>
+          <p className={styles.TrackBagContent}>We provide amazing offers & discounts.</p>
+        </div>
+
+
+      </div>
+    </main>
   )
 }
 
