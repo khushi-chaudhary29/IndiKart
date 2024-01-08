4c991d64f0143f3d2964231b7e6ebda4b5310469
Khushi Chaudhary <khushichdry2912@gmail.com>
2024-01-08 23:24:43 +0530

    add product and update product by Khushi

--- src/app/addproduct/page.js
+++ src/app/addproduct/page.js
@@ -3,7 +3,22 @@ import styles from '../page.module.css'
 
 const AddProduct = () => {
   return (
-    <main className='main'>AddProduct</main>
+    <main className='main'>
+      <div className={styles.AddProductContainer}>
+        <h3 className={styles.AddProductHeading}>Add Product</h3>
+        <input className={styles.AddProductAbout} type="text" name="txt_name" placeholder='Product title'></input>
+        <br></br>
+        <input className={styles.AddProductAbout} type="text" name="txt_name" placeholder='Product price'></input>
+        <br></br>
+        <input className={styles.AddProductAbout} type="text" name="txt_name" placeholder='Product imageUrl'></input>
+        <br></br>
+        <input className={styles.AddProductAbout} type="text" name="txt_name" placeholder='Product category'></input>
+        <br></br>
+        <textarea className={styles.AddProductTextArea} name="txt_qry" rows="10" cols="36" placeholder='Product desc'></textarea>
+        <br></br>
+        <button className={styles.AddProductButton}>Add Product</button>
+      </div>
+    </main>
   )
 }
 
--- src/app/updateproduct/page.js
+++ src/app/updateproduct/page.js
@@ -2,7 +2,22 @@ import React from 'react'
 import styles from '../page.module.css'
 const UpdateProduct = () => {
   return (
-    <main className='main'>UpdateProduct</main>
+    <main className='main'>
+      <div className={styles.AddProductContainer}>
+        <h3 className={styles.AddProductHeading}>Update Product</h3>
+        <input className={styles.AddProductAbout} type="text" name="txt_name" placeholder='Product title'></input>
+        <br></br>
+        <input className={styles.AddProductAbout} type="text" name="txt_name" placeholder='Product price'></input>
+        <br></br>
+        <input className={styles.AddProductAbout} type="text" name="txt_name" placeholder='Product imageUrl'></input>
+        <br></br>
+        <input className={styles.AddProductAbout} type="text" name="txt_name" placeholder='Product category'></input>
+        <br></br>
+        <textarea className={styles.AddProductTextArea} name="txt_qry" rows="10" cols="36" placeholder='Product title'></textarea>
+        <br></br>
+        <button className={styles.AddProductButton}>Update Product</button>
+      </div>
+    </main>
   )
 }
 
