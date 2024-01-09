9b7a62ec1a9cd54f5c01e7d288492075542080ab
Mayank Upadhyay <140595877+Mayankupadhyay-25@users.noreply.github.com>
2024-01-09 19:26:02 +0530

    creat modal by Mayank

--- src/app/order/page.js
+++ src/app/order/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from '../page.module.css'
+import Modal from '@/components/Modal'
 
 const Order = () => {
   return (
     <main className='main'>
      Order
+     <Modal/>
     </main>
   )
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,8 +3,22 @@ import styles from '../app/page.module.css'
 
 const Modal = () => {
   return (
-    <div>Modal</div>
-  )
+    <div>
+      <div className={styles.container_form}>
+        <form>
+          <p className="abc">Enter Full Name</p>
+          <input type="text" className={styles.fullname}/>
+            <p>Enter Full Address</p>
+            <input type="text" className={styles.address}/>
+              <p>Enter Pincode</p>
+              <input type="number" className={styles.pinnumber}/>
+                <p>Enter Mobile number</p>
+                <input type="number" className={styles.mobile_no}/>
+                </form>
+                <button class={styles.button}>Buy Now</button>
+              </div>
+            </div>
+            )
 }
 
-export default Modal
+            export default Modal
