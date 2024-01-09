7b54658634174f5172bae26354b00ece036b9d54
Mayank Upadhyay <140595877+Mayankupadhyay-25@users.noreply.github.com>
2024-01-09 19:53:54 +0530

    create contact by Mayank

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,8 +3,19 @@ import styles from '../page.module.css'
 
 const Contact = () => {
   return (
-    <main className='main'>Contact</main>
-  )
+    <div className={styles.containerform}>
+      <h3 className={styles.heading}>Contact Us</h3>
+      <form action="#" method="post">
+        <input type="text" className={styles.fullname} placeholder="full Name"/>
+          <input type="email" className={styles.email} placeholder="Email"/>
+            <input type="number" className={styles.number} placeholder="Phone"/>
+              <input type="text" className={styles.query} placeholder="Any comment or your query"/>
+              </form>
+              <button className={styles.button}>Send</button>
+              <p className={styles.contactus}>You can also contact us at 181-1711-322</p>
+            
+            </div>
+            )
 }
 
-export default Contact
+            export default Contact
